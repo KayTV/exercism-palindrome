3e396a1cd72264bea15dfae931f3e9f401a5daf1
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-01-14 09:24:40 -0700

    exercise with comments

--- prime-factors.js
+++ prime-factors.js
@@ -1,12 +1,19 @@
+//define the function primeNumbers
 function primeFactors (num) {
+  //define the variables input which is equal to the function num and factors as an empty array
     var input = num;
     var factors = [];
+    //create a for loop in which it starts at 2 and increases according to the input variable
     for (var i = 2; i <= input; i++) {
+      //a while loop defined. while the input var has a remainder of 0 and 1, go through the loop
         while(input % i === 0 && input !== 1) {
+          //if both arguments are true, push the number in the empty array and input is now equal to
+          //input divided by var i
             factors.push(i);
             input = input/i;
         }
     }
+    //after loop is done, return the empty array now full of prime numbers
     return factors;
 }
 
